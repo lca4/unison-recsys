3d736c9d212283a66a52a11295ffccea8d5a892b
Marc Bourqui <marc.bourui@epfl.ch>
2013-03-25 18:27:09 +0100

    Creates solo_views and first request handler
    Creates solo_views to handle request for the single-user mode.
    
    Supported request:
    * get_playlist

---
+++ scripts/dev/solo_views.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""Single-user-related views."""
+
+
+import datetime
+import hashlib
+import helpers
+import libunison.geometry as geometry
+import libunison.predict as predict
+import random
+import time
+
+from constants import errors, events
+from flask import Blueprint, request, g, jsonify
+from libentry_views import set_rating
+from libunison.models import User, Group, Track, LibEntry, GroupEvent
+from operator import itemgetter
+from storm.expr import Desc, In
+
+
+group_views = Blueprint('solo_views', __name__)
+
+
+@solo_views.route('/<int:gid>/playlist', methods=['GET'])
+@helpers.authenticate(with_user=True)
+def get_playlist(gid):
+    """Get a playlist"""
+    type = request.form['type']
+    seed = request.form['seed']
+    filter = request.form['filter']
+    size = request.form['size']
+    sort = request.form['sort']
+    
+    if type is None:
+        raise helpers.BadRequest(errors.MISSING_FIELD,
+                "type of seed not specified")
+    elif type is None:
+        raise helpers.BadRequest(errors.MISSING_FIELD,
+                "seed not specified")
+    
+    playlist = plgenerator.plgenerator(gid, type, seed, filter, size, sort)
+    # Craft the JSON response.
+    if playlist is not None:
+        tracks = list()
+        for entry in playlist:
+            tracks.append({
+              'artist': entry.track.artist,
+              'title': entry.track.title,
+              'local_id': entry.local_id,
+            })
+        return jsonify(tracks=tracks)
+    return None
