3f7dbf56a4840ac68c1868b913b953b97fd1d62b
Lucas Maystre <lucas@maystre.ch>
2013-06-10 13:12:47 +0200

    Improved cleanup.py, now handles automatic groups.
    Improvements to the script `cleanup.py` that cleans up the groups that are
    inactive. Now it also takes care of correctly clean up automatic groups.

--- scripts/dbutils/cleanup.py
+++ scripts/dbutils/cleanup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import argparse
 import datetime
 import time
@@ -10,10 +9,11 @@ from storm.locals import *
 from storm.expr import Desc
 
 
-def cleanup(interval, verbose):
+def cleanup(interval, verbose, is_dry_run):
     thresh = datetime.datetime.fromtimestamp(
             time.time() - interval * 60 * 60)
     store = uutils.get_store()
+    count = 0
     for group in store.find(Group, Group.is_active == True):
         # Iterate over all active groups.
         last_event = store.find(GroupEvent, GroupEvent.group
@@ -21,12 +21,19 @@ def cleanup(interval, verbose):
         if (last_event is None and group.created < thresh
                 or last_event is not None and last_event.created < thresh):
             # Group is old and has no event, or last event is old.
+            count += 1
             if verbose:
                 print "deactivating group %d (%s)" % (group.id, group.name)
-            group.is_active = False
-            for user in group.users:
-                user.group = None
+            if not is_dry_run:
+                group.is_active = False
+                if group.automatic:
+                    for cluster in group.clusters:
+                        # There should be only one cluster.
+                        cluster.group = None
+                for user in group.users:
+                    user.group = None
     store.commit()
+    return count
 
 
 def _parse_args():
@@ -34,9 +41,12 @@ def _parse_args():
     # Cutoff interval, number of hours since last event in group.
     parser.add_argument('interval', type=int)
     parser.add_argument('--verbose', action='store_true')
+    parser.add_argument('--dry-run', action='store_true')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     args = _parse_args()
-    cleanup(args.interval, args.verbose)
+    count = cleanup(args.interval, args.verbose, args.dry_run)
+    now = time.strftime("%a %d %b %y %H:%M:%S")
+    print "%s - cleaned up %d groups" % (now, count)
